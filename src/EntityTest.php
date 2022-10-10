6c24a45291fdee33d48f0afc07bb4fb99d563260
Lucas Johnston <l.johnston@phitech.consulting>
2022-10-10 14:56:34 +0200

    v1.1beta9 [Autodiscover in Laravel]

--- src/EntityTest.php
+++ src/EntityTest.php
@@ -8,4 +8,8 @@ class EntityTest extends Entity
     public function __construct($id = null) {
         parent::__construct('entity_test');
     }
+
+    public function output_test_message() {
+        dd("It works!");
+    }
 }
