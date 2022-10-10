23aecdb5d2b373b10cc3970c100db0af215d2e46
Lucas Johnston <l.johnston@phitech.consulting>
2022-10-10 14:25:02 +0200

    v1.1beta [Autodiscover in Laravel]

--- src/Entities/Entity.php
+++ src/Entities/Entity.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Entities;
+namespace Phitech\Entities;
 
 class Entity {
     public $main = "";
---
+++ src/EntitiesServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Phitech\Entities;
+
+use Illuminate\Support\ServiceProvider;
+
+class 
+{
+	
+	public function boot() {
+		dd("it works!");
+	}
+	
+	public function register() {
+		
+	}
+}
